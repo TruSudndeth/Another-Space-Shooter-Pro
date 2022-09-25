af93d503d8fd07fef85ab756bbacbb235fa04929
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-09-25 15:42:28 -0700

    Challenge: User Input added Horizontal and vertical movment with speed

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private MyBaseInputs playerInputs;
+    private Vector2 movePlayer;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private InputAction WSAD; //Debugit unSerialize private fild
+
+    // Add Some movement interperlation Plz (Mooth out)
     void Start()
     {
-        
+        playerInputs = new(); //Create a new instance of MyBaseInputs
+        WSAD = playerInputs.Player.Move;
+        WSAD.Enable();
+    }
+    private void OnDestroy()
+    {
+        WSAD.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = WSAD.ReadValue < Vector2> ();
+        if (movement == Vector2.zero) // a bit redundent :)
+        {
+            movement = Vector2.zero;
+        }
+        movePlayer = movement;
+    }
+
+    private void FixedUpdate()
+    {
+        // Might not need to set translate if there is no input hmmmm
+        transform.Translate(movePlayer * speed * Time.fixedDeltaTime, Space.World); //Moves the transfomr in the direction and distance of translation
     }
 }
