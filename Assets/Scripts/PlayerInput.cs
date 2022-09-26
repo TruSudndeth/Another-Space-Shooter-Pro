8839f406bb2905280d700b8d3fb711478455eda0
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-09-25 18:01:15 -0700

    Challenge Player Bounds

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -10,10 +10,18 @@ public class PlayerInput : MonoBehaviour
     private Vector2 movePlayer;
     [SerializeField] private float speed = 1;
     [SerializeField] private InputAction WSAD; //Debugit unSerialize private fild
+    [SerializeField] private Transform myCamera; //set in the inspector.
+    private float cameraOrthoSize = 5;
+    private float cameraAspectRatio = 1.7777778f;
+    private float xBounds = 0;
+    private float yBounds = 0;
 
     // Add Some movement interperlation Plz (Mooth out)
     void Start()
     {
+        cameraOrthoSize = myCamera.GetComponent<Camera>().orthographicSize;
+        xBounds = cameraOrthoSize * cameraAspectRatio;
+        yBounds = cameraOrthoSize;
         playerInputs = new(); //Create a new instance of MyBaseInputs
         WSAD = playerInputs.Player.Move;
         WSAD.Enable();
@@ -26,10 +34,27 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Bounds set
+        //Fix bound clipping due to frame rate.
         Vector2 movement = WSAD.ReadValue < Vector2> ();
-        if (movement == Vector2.zero) // a bit redundent :)
+        if (movement != Vector2.zero) // Not so redundent now :)
         {
-            movement = Vector2.zero;
+            if (transform.position.x > xBounds && movement.x > 0)
+            {
+                movement.x = 0;
+            }else if(transform.position.x < -xBounds && movement.x < 0)
+            {
+                movement.x = 0;
+            }
+
+            if(transform.position.y > yBounds && movement.y > 0)
+            {
+                movement.y = 0;
+            }
+            else if(transform.position.y < -yBounds && movement.y < 0)
+            {
+                movement.y = 0;
+            }
         }
         movePlayer = movement;
     }
