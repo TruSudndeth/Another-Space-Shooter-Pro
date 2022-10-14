cd4798531f61ffc978a2e9551f8dcca0961d0859
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-10-13 22:19:46 -0700

    Player had a Game over when 3 lives are lost complete

--- Assets/UI Layout/UI.cs
+++ Assets/UI Layout/UI.cs
@@ -9,12 +9,15 @@ public class UI : MonoBehaviour
     private VisualElement _healthBar;
     private int _score = 0;
     private Label _scoreLabel;
-
-
-
-    
+    [Space]
+    [SerializeField] private float _gameOverFlashTime = 1.0f;
+    private Label _gameOver;
+    private float _gameOverTime = 0.0f;
+    private bool _isGameOver = false;
+    [Space]
     [SerializeField] private List<Sprite> _healthStatus;
     private List<StyleBackground> _healthStatusStyle;
+
     private void Awake()
     {
         _healthStatusStyle = new(4);
@@ -27,6 +30,7 @@ public class UI : MonoBehaviour
     {
         PlayerInput.Score += UpdateScore;
         PlayerInput.UpdateHealth += UpdateHealth;
+        PlayerInput.gameOver += GameOver;
 
         UpdateScore(0);
     }
@@ -34,6 +38,16 @@ public class UI : MonoBehaviour
     {
         PlayerInput.Score -= UpdateScore;
         PlayerInput.UpdateHealth -= UpdateHealth;
+        PlayerInput.gameOver -= GameOver;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_gameOverTime + _gameOverFlashTime < Time.time && _isGameOver)
+        {
+            _gameOverTime = Time.time;
+            _gameOver.style.visibility = _gameOver.style.visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
+        }
     }
 
     private void UpdateScore(int points)
@@ -43,6 +57,11 @@ public class UI : MonoBehaviour
         //label is not being found (Script Execution order)
         _scoreLabel.text = "Score: " + _score;
     }
+    private void GameOver()
+    {
+        //create a on off logic over time
+        _isGameOver = true;
+    }
 
     private void UpdateHealth(int health)
     {
@@ -51,10 +70,14 @@ public class UI : MonoBehaviour
     
     private void OnEnable()
     {
+        //Reff
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         _healthBar = root.Q<VisualElement>("HealthBar");
-        _healthBar.style.backgroundImage = _healthStatusStyle[_healthStatus.Count - 1];
-        
         _scoreLabel = root.Q<Label>("Score");
+        _gameOver = root.Q<Label>("GameOver");
+
+        //Set
+        _healthBar.style.backgroundImage = _healthStatusStyle[_healthStatus.Count - 1];
+        _gameOver.visible = false;
     }
 }
