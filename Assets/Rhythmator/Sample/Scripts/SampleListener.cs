00f69d48e83eabddbefab738bbe9565555ab7de0
TruSudndeth <72487248+TruSudndeth@users.noreply.github.com>
2022-10-28 11:36:08 -0700

    Added Package Rhythmator for Aduio driven events

---
+++ Assets/Rhythmator/Sample/Scripts/SampleListener.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleListener : RhythmListener {
+
+    public GameObject[] logo;
+    public GameObject noteRight;
+    public GameObject noteLeft;
+
+    Vector3[] logoScale;
+
+    private void Start() {
+        logoScale = new Vector3[logo.Length];
+        for (int i = 0; i < logo.Length; i++) {
+            logoScale[i] = logo[i].transform.localScale;
+        }
+
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void Update() {
+        for (int i = 0; i < logo.Length; i++) {
+            Vector3 logoScl = logo[i].transform.localScale;
+            logoScl += (logoScale[i] - logoScl) / 10f * Time.deltaTime * 60f;
+            logo[i].transform.localScale = logoScl;
+        }
+    }
+
+    public override void BPMEvent(RhythmEventData data) {
+        logo[2].transform.localScale = logoScale[2] * 1.2f;
+    }
+
+    public override void RhythmEvent(RhythmEventData data) {
+        if (data.layer.layerName == "h5") {
+            if (data.objects.Count > 0) {
+                int d = (int)data.objects[0];
+                logo[d == 52 ? 0 : 1].transform.localScale = logoScale[d == 52 ? 0 : 1] * 1.2f;
+            }
+            return;
+        }
+
+        float y = 0;
+        if (data.objects.Count > 0) {
+            y = ((int)data.objects[0] - 65) / 2f;
+        }
+        bool h1 = (data.layer.layerName == "h1");
+        Instantiate(h1 ? noteRight : noteLeft, transform.position + Vector3.up * y + Vector3.right * (h1 ? -8 : 8), Quaternion.identity);
+    }
+
+
+}
---
+++ Assets/Rhythmator/Scripts/Tools/MarkupTool.cs
@@ -0,0 +1,82 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MarkupTool : RhythmTool
+{
+
+    Rhythm.Markup beignCreated;
+    float initialCreationPosition;
+
+    public MarkupTool(RhythmEditor editor) :  base(editor)
+    {
+    }
+
+    // This is called for each window that your tool is active in. Put the functionality of your tool here.
+    public override void GUI(EditorWindow window)
+    {
+        EditorGUIUtility.AddCursorRect(RhythmEditor.timelineController.GetWaveformDrawer().GetRect(), Event.current.control ? MouseCursor.ArrowMinus : MouseCursor.ArrowPlus);
+
+        WaveformDrawer wd = RhythmEditor.timelineController.GetWaveformDrawer();
+        MarkupsDrawer md = RhythmEditor.timelineController.GetMarkupsDrawer();
+
+        if (wd.GetRect().Contains(Event.current.mousePosition)) {
+            if (Event.current.button == 0) {
+                if (!Event.current.control) {
+                    if (Event.current.type == EventType.MouseDown) {
+                        if (editor.rhythm.layers.Count == 0) {
+                            editor.rhythm.layers.Add(new Rhythm.MarkupLayer() {
+                                layerName = "Default",
+                                color = Color.blue,
+                                visible = true,
+                                markups = new List<Rhythm.Markup>()
+                            });
+                            Debug.Log("Non existing layer, creating");
+                        }
+                        Undo.RecordObject(editor.rhythm, "Add markup");
+                        Rhythm.Markup m = new Rhythm.Markup() { timer = wd.PixelsToSeconds(Event.current.mousePosition.x + wd.GetScroll()), length = 0, additionalParameters = new List<RhythmEventData.Primitive>() };
+                        editor.rhythm.layers[md.LayerHover()].markups.Add(m);
+
+                        editor.rhythm.layers[md.LayerHover()].markups.Sort((a, b) => (int)((a.timer - b.timer) * 100f));
+
+                        beignCreated = m;
+                        initialCreationPosition = m.timer;
+                        Debug.Log("Created markup at position " + m.timer);
+                    }
+
+                    if (Event.current.type == EventType.MouseDrag) {
+                        if (beignCreated != null) {
+                            float at = wd.PixelsToSeconds(Event.current.mousePosition.x + wd.GetScroll());
+                            if (at > initialCreationPosition) {
+                                beignCreated.length = at - initialCreationPosition;
+                            }
+                            else {
+                                beignCreated.length = initialCreationPosition - at;
+                                beignCreated.timer = at;
+                            }
+                        }
+                    }
+                }
+                else {
+                    if (Event.current.type == EventType.MouseDrag || Event.current.type == EventType.MouseDown) {
+                        Rhythm.Markup over = null;
+                        if (md.LayerHover() != -1) {
+                            foreach (Rhythm.Markup m in editor.rhythm.layers[md.LayerHover()].markups) {
+                                if (md.GetMarkupRect(m).Contains(Event.current.mousePosition)) {
+                                    over = m;
+                                    break;
+                                }
+                            }
+                            Undo.RecordObject(editor.rhythm, "Remove Markups");
+                            editor.rhythm.layers[md.LayerHover()].markups.Remove(over);
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
